c6571bf8f2bfaf1421466074e5f99ec0fd7dc542
pablo <pavm.met@gmai.com>
2022-10-08 12:09:13 -0500

    Recursos publicos

--- components/Encabezado.vue
+++ components/Encabezado.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
-        Logo, Mi tienda, Links, Usuario
+        <NuxtLink to="/">
+            <img src="/img/logo.png" alt="logo"/>
+        </NuxtLink>
+        <span/>
+        Mi tienda
+        <span/>
+        Links: 
+        <NuxtLink to="/categorias/deportes">Deportes</NuxtLink>
+        <span/>
+        Usuario
     </div>
 </template>
+
+<style scoped>
+span{
+  width: 25px;
+  display: inline-block;
+}
+</style>
